31f350ca061a9cdf9346a863860a62ea2037317a
Skukar <jambubatu2580@gmail.com>
2025-01-12 20:12:58 +0700

    premium

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "Bjhon Vpn Premium Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script @BjhonStore" | lolcat
+echo "                   Author Script Bjhon Vpn Premium" | lolcat
+echo "-----------------------------------------------------"
---
+++ menu/menu1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sS "https://raw.githubusercontent.com/BjhonStore/Premium/main/menu/dashboard.sh" | bash
